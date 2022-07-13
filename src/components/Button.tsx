7d200f0603547d70e5b0aa76c2f26499e9377f9a
alberto jauregui dominguez <ajauregui@wtelecom.es>
2022-07-14 00:06:20 +0200

    add modals and one more button color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,15 +6,25 @@ interface ButtonProps
     HTMLButtonElement
   > {
   text: string;
+  color?: "white" | "red";
 }
 
-export const Button: React.FC<ButtonProps> = ({ text, ...otherProps }) => {
+export const Button: React.FC<ButtonProps> = ({
+  text,
+  color = "red",
+  ...otherProps
+}) => {
+  const className = (color: string): string => {
+    switch (color) {
+      case "white":
+        return "text-gray-400 bg-white border border-gray-300 hover:bg-gray-300 focus:outline-none focus:ring-4 focus:ring-gray-500 font-medium rounded-full text-sm px-5 py-2 text-center dark:bg-custom-red-600 dark:hover:bg-custom-red-700 dark:focus:ring-custom-red-900";
+      case "red":
+      default:
+        return "text-white bg-custom-red-600 hover:bg-custom-red-800 focus:outline-none focus:ring-4 focus:ring-custom-red-300 font-medium rounded-full text-sm px-5 py-2 text-center dark:bg-custom-red-600 dark:hover:bg-custom-red-700 dark:focus:ring-custom-red-900";
+    }
+  };
   return (
-    <button
-      type="button"
-      className="text-white bg-custom-red-600 hover:bg-custom-red-800 focus:outline-none focus:ring-4 focus:ring-custom-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-custom-red-600 dark:hover:bg-custom-red-700 dark:focus:ring-custom-red-900"
-      {...otherProps}
-    >
+    <button type="button" className={className(color)} {...otherProps}>
       {text}
     </button>
   );
