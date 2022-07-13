dd7322f149a197365217dd4991b27c4e50ee71d8
alberto jauregui dominguez <ajauregui@wtelecom.es>
2022-07-13 23:48:03 +0200

    remove margin buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,7 +12,7 @@ export const Button: React.FC<ButtonProps> = ({ text, ...otherProps }) => {
   return (
     <button
       type="button"
-      className="text-white bg-custom-red-600 hover:bg-custom-red-800 focus:outline-none focus:ring-4 focus:ring-custom-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-custom-red-600 dark:hover:bg-custom-red-700 dark:focus:ring-custom-red-900"
+      className="text-white bg-custom-red-600 hover:bg-custom-red-800 focus:outline-none focus:ring-4 focus:ring-custom-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-custom-red-600 dark:hover:bg-custom-red-700 dark:focus:ring-custom-red-900"
       {...otherProps}
     >
       {text}
