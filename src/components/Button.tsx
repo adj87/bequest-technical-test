e963020f47cb0f9b11e049a2bee5bb62bc62266f
alberto jauregui dominguez <ajauregui@wtelecom.es>
2022-07-14 23:49:19 +0200

    add disabled buttons styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -17,10 +17,10 @@ export const Button: React.FC<ButtonProps> = ({
   const className = (color: string): string => {
     switch (color) {
       case "white":
-        return "text-gray-400 bg-white border border-gray-300 hover:bg-gray-300 focus:outline-none focus:ring-4 focus:ring-gray-500 font-medium rounded-full text-sm px-5 py-2 text-center dark:bg-custom-red-600 dark:hover:bg-custom-red-700 dark:focus:ring-custom-red-900";
+        return "text-gray-400 bg-white border border-gray-300 hover:enabled:bg-gray-300 focus:outline-none focus:ring-4 focus:ring-gray-500 font-medium rounded-full text-sm px-5 py-2 text-center disabled:opacity-50";
       case "red":
       default:
-        return "text-white bg-custom-red-600 hover:bg-custom-red-800 focus:outline-none focus:ring-4 focus:ring-custom-red-300 font-medium rounded-full text-sm px-5 py-2 text-center dark:bg-custom-red-600 dark:hover:bg-custom-red-700 dark:focus:ring-custom-red-900";
+        return "text-white bg-custom-red-600 hover:enabled:bg-custom-red-800 focus:outline-none focus:ring-4 focus:ring-custom-red-300 font-medium rounded-full text-sm px-5 py-2 text-center disabled:opacity-50";
     }
   };
   return (
