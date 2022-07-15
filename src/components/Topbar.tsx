18b7fcedf9998ad6c021be928f77034fe456f4f2
alberto jauregui dominguez <ajauregui@wtelecom.es>
2022-07-15 17:59:15 +0200

    some styles modifications in topbar

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 export const Topbar: React.FC = () => {
   return (
-    <nav className="flex justify-between p-2">
-      <img src="/logo.svg" alt="logo" className="w-24" />
-      <span className="text-sm text-gray-400">mr_jaurewi</span>
+    <nav className="flex justify-between p-2 bg-white">
+      <img src="/logo.svg" alt="logo" className="w-28 cursor-pointer" />
+      <span className="text-sm sm:text-lg text-gray-400 cursor-default">
+        mr_jaurewi
+      </span>
     </nav>
   );
 };
