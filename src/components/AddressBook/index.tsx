1fb1bb4d980a8b4554378a3380a01977c66c2ada
alberto jauregui dominguez <ajauregui@wtelecom.es>
2022-07-15 18:32:28 +0200

    add edit add and remove button

--- src/components/AddressBook/index.tsx
+++ src/components/AddressBook/index.tsx
@@ -27,16 +27,33 @@ export const AddressBook: React.FC<AddressBooksProps> = ({
   value
 }) => {
   return (
-    <InputRadioGroup<Address>
-      name={name}
-      options={options}
-      optionLabel={(address): React.ReactElement => (
-        <span className="ml-4">{address.country}</span>
-      )}
-      valueKey={"id"}
-      value={value}
-      label={"Select address"}
-      onChange={onChange}
-    />
+    <>
+      <InputRadioGroup<Address>
+        name={name}
+        options={options}
+        optionLabel={(address): React.ReactElement => (
+          <>
+            <span className="ml-4 font-bold inline-block w-24">
+              {address.country}
+            </span>
+            <span className="ml-4 text-md text-gray-600">{`${address.line1}, ${address.line2}, ${address.line3}`}</span>
+            <span className="text-custom-green cursor-pointer ml-4 hover:">
+              Edit
+            </span>
+            <span className="text-custom-green cursor-pointer ml-2">
+              Remove
+            </span>
+          </>
+        )}
+        valueKey={"id"}
+        value={value}
+        label={"Select address"}
+        onChange={onChange}
+      />
+      <span className="text-custom-green mt-8 inline-block cursor-pointer">
+        {" "}
+        Add more address
+      </span>
+    </>
   );
 };
