f92744615e64f7f0361e78a97a055c1a0a0c304c
alberto jauregui dominguez <ajauregui@wtelecom.es>
2022-07-15 18:02:33 +0200

    add home page to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,9 @@
-import React, { useState } from "react";
-import "./App.css";
-import "./index.css";
-import { InputRadioGroup, InputText } from "./components";
-
-interface Form {
-  radio: string;
-  text: string;
-}
+import React from "react";
+import "./index.scss";
+import { Home } from "./pages/Home";
 
 const App: React.FC = (props) => {
-  const [values, setValues] = useState<Form>({ radio: "1", text: "string" });
-  console.log(values);
-  return (
-    <>
-      <InputRadioGroup
-        name="radio"
-        value={values.radio}
-        options={[
-          { label: "opcion 1", value: "1" },
-          { label: "opcion 2", value: "2" }
-        ]}
-        onChange={(name, val): void => setValues({ ...values, [name]: val })}
-        label={"label test"}
-        customLabel={<span>qw</span>}
-      />
-      <InputText
-        label="Label input"
-        required={true}
-        onChange={(name, val): void => setValues({ ...values, [name]: val })}
-        name="text"
-        value={values.text}
-      />
-    </>
-  );
+  return <Home />;
 };
 
 export default App;
