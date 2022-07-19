9a8697eeff9340147e11c45774e68614186bc605
alberto jauregui dominguez <ajauregui@wtelecom.es>
2022-07-19 14:39:14 +0200

    align input radios

--- src/components/AddressBook/OptionLabel.tsx
+++ src/components/AddressBook/OptionLabel.tsx
@@ -9,18 +9,22 @@ interface OptionLabelProps {
 const OptionLabel: React.FC<OptionLabelProps> = (props) => {
   const { address, onDelete, onEdit } = props;
   const { line1, line2, line3, country } = address;
-  const addComma = (string: string) => string && `${string},`;
+  const addComma = (string: string) => string && `${string}, `;
   return (
     <>
       {" "}
-      <span className="ml-4 font-bold inline-block w-30">{line1}</span>
-      <span className="ml-4 text-md text-gray-600 hidden xs:inline">
+      <span className="ml-4 font-bold inline-block max-w-32 md:max-w-44 truncate">
+        {line1}
+      </span>
+      <span className="ml-4 text-md text-gray-600 hidden xs:inline-block mr-2">
         {addComma(line2)}
       </span>
-      <span className="text-md text-gray-600 hidden md:inline">
+      <span className="text-md text-gray-600 hidden md:inline-block mr-2">
         {addComma(line3)}
       </span>
-      <span className="text-md text-gray-600 hidden md:inline">{country}</span>
+      <span className="text-md text-gray-600 hidden md:inline-block mr-2">
+        {country}
+      </span>
       <span className={"span-green ml-4"} onClick={() => onEdit(address)}>
         Edit
       </span>
